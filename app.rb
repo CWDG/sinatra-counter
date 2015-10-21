3c8ede25593fa477869bd1fdf0359213512ff1a6
Erik Biberstein <ebiberst@gmail.com>
2015-10-20 23:34:31 -0400

    First update
    Just committing your version of the code, Jon.  I will try to try an
    alternative route to the solution or add new things this weekend when I
    have time.

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 set :port, 3000
-
+set :count, 0
 # Your code here!
 get '/' do
   # Replace the contents of this method
-  "Hello world!"
+  settings.count += 1
+  "Count: #{settings.count}"
+
 end
